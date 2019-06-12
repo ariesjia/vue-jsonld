89232a12355629129f11ed76076536ede3050786
areschen <ariesjia00@hotmail.com>
2019-06-12 21:33:06 +0800

    fix: fix typing

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,16 @@
-import Vue from "vue";
+import { VueConstructor } from "vue";
 import createMixin from "./mixin"
 import createInstance from "./jsonld"
 import { Option } from "./typing"
 
 interface Install {
-  (vue: Vue, options: Option): void
+  (vue: VueConstructor, options: Option): void
   installed?: boolean
 }
 
-export const install: Install = (vue: Vue, options) => {
+export const install: Install = (vue: VueConstructor, options) => {
   if (!install.installed) {
-    // @ts-ignore
     vue.prototype.$jsonld = createInstance(options)
-    // @ts-ignore
     vue.mixin(createMixin(vue, options))
     install.installed = true
   }
